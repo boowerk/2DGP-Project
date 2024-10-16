43e4620c86fabaf770d9762cffe74027cb87d70d
boowerk <back20044@gmail.com>
2024-10-16 21:27:15 +0900

    ESC 키와 윈도우 x 클릭으로 창 닫기 구현 완료

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from pico2d import *
 
 def handle_events():
+    global running
+    events = get_events()
+    
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
     pass
 
 def reset_world():

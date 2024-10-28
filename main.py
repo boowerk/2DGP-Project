49fc4b997c8ccc198f4cebb678ef7fa02cbab2de
boowerk <back20044@gmail.com>
2024-10-28 23:41:35 +0900

    맵 타일 출력이 잘 되는지 확인

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from pico2d import *
 
+class Map:
+    def __init__(self):
+        self.image = load_image('tiles.png')
+
+    def draw(self):
+        self.image.draw(400, 30)
+
+    def upadate(self):
+        pass
+
+
 def handle_events():
     global running
     events = get_events()
@@ -13,12 +24,29 @@ def handle_events():
     pass
 
 def reset_world():
+    global running
+    global world
+    global map
+
+    running = True
+    world = []
+
+    map = Map()
+    world.append(map)
+
+    pass
+
+def update_world():
+    for o in world:
+        o.upadate()
     pass
 
 def render_world():
-    pass
+    clear_canvas()
+    for o in world:
+        o.draw()
+    update_canvas()
 
-running = True
 
 open_canvas()
 
@@ -28,7 +56,7 @@ reset_world()
 while running:
     #game logic
     handle_events()
-    update_canvas()
+    update_world()
     render_world()
 
 close_canvas

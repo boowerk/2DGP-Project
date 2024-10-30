9e91eef4fbac96bd32b4f7e54b3ed7863e7f9b63
boowerk <back20044@gmail.com>
2024-10-31 00:23:23 +0900

    Idle에서 Wait 상태로 들어가기 구현 및 Wait 애니메이션 구현
    Idle에서 3초 지나면 Wait

--- king.py
+++ king.py
@@ -1,23 +1,25 @@
 from pico2d import load_image, get_time
 
-from state_machine import StateMachine
+from state_machine import StateMachine, time_out
 
 
 class Idle:
     @staticmethod
-    def enter(king):
-        king.dir = 0
+    def enter(king, e):
+        king.dir = 0    # 정지상태
         king.frame = 8
 
         king.start_time = get_time()
         pass
 
     @staticmethod
-    def exit(king):
+    def exit(king, e):
         pass
 
     @staticmethod
     def do(king):
+        if get_time() - king.start_time > 3:
+            king.state_machine.add_event(('TIME_OUT', 0))
         pass
 
     @staticmethod
@@ -27,19 +29,36 @@ class Idle:
 
 class Wait:
     @staticmethod
-    def enter(king):
+    def enter(king, e):
+        king.frame = 8
+        king.frame_step = 1
+        king.frame_timer = 0
         pass
 
     @staticmethod
-    def exit(king):
+    def exit(king, e):
+        king.frame = 8
+        king.frame_step = 1
         pass
 
     @staticmethod
     def do(king):
+        king.frame_timer += 0.01
+
+        if king.frame_timer >= king.frame_delay:
+            king.frame_timer = 0
+
+            if king.frame == 10:
+                king.frame_step = -1
+            elif king.frame == 8:
+                king.frame_step = 1
+
+            king.frame += king.frame_step
         pass
 
     @staticmethod
     def draw(king):
+        king.image.clip_draw(king.frame * 64, 0, 64, 64, king.x, king.y, 192, 192)
         pass
 
 
@@ -48,10 +67,18 @@ class King:
         self.x, self.y = 400, 356
         self.dir = 0
         self.frame = 8  # 정지 상태
+        self.frame_step = 1 # 프레임의 증가 또는 감소
+        self.frame_delay = 0.1  # 프레임 전환 간격
         self.image = load_image('king.png')
 
         self.state_machine = StateMachine(self)
         self.state_machine.start(Idle)
+        self.state_machine.set_transitions(
+            {
+                Idle: {time_out: Wait}
+
+            }
+        )
 
     def draw(self):
         self.state_machine.draw()
@@ -60,3 +87,8 @@ class King:
     def update(self):
         self.state_machine.update()
         pass
+
+    def handle_event(self, event):
+        self.state_machine.add_event(
+            ('INPUT', event)
+        )

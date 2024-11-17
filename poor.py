a607a3253cd617c4449b12e4c3fb9ff9da1e49f5
boowerk <back20044@gmail.com>
2024-11-17 19:31:32 +0900

    poor 객체가 Wait state의 애니메이션을 한번만 수행하도록 수정

--- poor.py
+++ poor.py
@@ -1,14 +1,16 @@
-from pico2d import load_image
+from pico2d import load_image, get_time
 
 import game_framework
-from state_machine import StateMachine
+from state_machine import StateMachine, time_out
 
 
 class Idle:
     @staticmethod
     def enter(poor, e):
-        poor.dir = 0
+        poor.dir = 0    # 정지상태
         poor.frame = 0
+
+        poor.start_time = get_time()
         pass
 
     @staticmethod
@@ -17,6 +19,8 @@ class Idle:
 
     @staticmethod
     def do(poor):
+        if get_time() - poor.start_time > 3:
+            poor.state_machine.add_event(('TIME_OUT', 0))
         pass
 
     @staticmethod
@@ -27,6 +31,9 @@ class Idle:
 class Wait:
     @staticmethod
     def enter(poor, e):
+        poor.frame = 0
+        poor.frame_col = 1
+        poor.once = False
         pass
 
     @staticmethod
@@ -35,10 +42,27 @@ class Wait:
 
     @staticmethod
     def do(poor):
+        poor.frame_timer += game_framework.frame_time
+        if poor.frame_timer >= 0.3 and poor.once == False:  # 프레임 간격을 0.1초로 설정 (필요에 따라 조정 가능)
+            poor.frame = (poor.frame + 6) % 36
+            poor.frame_timer = 0.0  # 타이머 리셋
+
+            if poor.frame == 30:
+                poor.frame_col = 0
+                poor.frame = 0
+                poor.once = True
+
+        elif poor.frame_timer >= 0.3 and poor.once == True:
+            poor.frame = (poor.frame + 6) % 18
+            poor.frame_timer = 0.0  # 타이머 리셋
+
+            if poor.frame == 12:
+                poor.state_machine.add_event(('TIME_OUT', 0))
         pass
 
     @staticmethod
     def draw(poor):
+        poor.wait_image.clip_draw(poor.frame * 128, poor.frame_col * 128, 128, 128, poor.x, poor.y, 100, 100)
         pass
 
 class Walk:
@@ -69,6 +93,12 @@ class Poor:
         self.walk_image = load_image('npc_walk_sprite.png')
         self.state_machine = StateMachine(self)
         self.state_machine.start(Idle)
+        self.state_machine.set_transitions(
+            {
+                Idle: {time_out: Wait},
+                Wait: {time_out: Idle}
+            }
+        )
 
     def draw(self):
         self.state_machine.draw()

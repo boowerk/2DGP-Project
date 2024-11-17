23ec7aa534e5cd842cc3bc74e70ec0d38335a293
boowerk <back20044@gmail.com>
2024-11-17 13:18:26 +0900

    Coin의 애니메이션 구현 완료

--- coin.py
+++ coin.py
@@ -1,10 +1,20 @@
-class Coin:
+from pico2d import load_image
 
-    def __init__(self):
+
+class Coin:
+    image = None
+
+    def __init__(self, x, y):
+        if Coin.image == None:
+            Coin.image = load_image("coin.png")
+        self.x, self.y = x, y
+        self.frame = 0
         pass
 
     def draw(self):
+        self.image.clip_draw(self.frame * 10, 0, 10, 10, self.x, self.y)
         pass
 
     def update(self):
+        self.frame = (self.frame + 1) % 8
         pass

74559940e483129ecd7a80e4ca541695dd72c097
boowerk <back20044@gmail.com>
2024-11-17 13:26:26 +0900

    coin의 프레임 속도 조절

--- coin.py
+++ coin.py
@@ -1,5 +1,7 @@
 from pico2d import load_image
 
+import game_framework
+
 
 class Coin:
     image = None
@@ -8,13 +10,16 @@ class Coin:
         if Coin.image == None:
             Coin.image = load_image("coin.png")
         self.x, self.y = x, y
+        self.frame_timer = 0
         self.frame = 0
-        pass
 
     def draw(self):
         self.image.clip_draw(self.frame * 10, 0, 10, 10, self.x, self.y, 20, 20)
         pass
 
     def update(self):
-        self.frame = (self.frame + 1) % 8
+        self.frame_timer += game_framework.frame_time
+        if self.frame_timer >= 0.1:  # 프레임 간격을 0.1초로 설정 (필요에 따라 조정 가능)
+            self.frame = (self.frame + 1) % 8
+            self.frame_timer = 0.0  # 타이머 리셋
         pass

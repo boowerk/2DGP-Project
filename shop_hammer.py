9bebfa671045e44b008e5ad6ea0a46a6990d8e8f
boowerk <back20044@gmail.com>
2024-11-25 01:10:28 +0900

    shop_hammer 의 충돌 박스 생성 및 설정 완료

--- shop_hammer.py
+++ shop_hammer.py
@@ -1,15 +1,21 @@
-from pico2d import load_image
+from pico2d import load_image, draw_rectangle
 
 
 class Shop_hammer:
     def __init__(self, king):
         self.king = king
+        self.x, self.y = 1100, 350
         self.image = load_image("shop_hammer.png")
         pass
 
     def draw(self):
-        self.image.draw(1100 - self.king.camera_x, 350)
+        self.image.draw(self.x - self.king.camera_x, self.y)
+        draw_rectangle(*self.get_bb())
         pass
+
+    def get_bb(self):
+        # 충돌 박스 좌표 반환
+        return self.x - self.king.camera_x - 50, self.y - 100, self.x - self.king.camera_x + 50, self.y
 
     def update(self):
         pass

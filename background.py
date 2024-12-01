d047be5f794ff314b6ea77bb60c7e0137b7555ab
boowerk <back20044@gmail.com>
2024-12-01 15:07:39 +0900

    background가 무한으로 이어지게 설정

--- background.py
+++ background.py
@@ -6,19 +6,28 @@ class Background:
     rocks = None
 
     def __init__(self, king):
-        if Background.hills == None:
+        if Background.hills is None:
             Background.hills = load_image("backdrop_hills.png")
 
-        if Background.rocks == None:
+        if Background.rocks is None:
             Background.rocks = load_image("backdrop_rocks.png")
 
         self.king = king
-
-        pass
+        self.hills_y = 420
+        self.rocks_y = 440
+        self.image_width = 1792  # 배경 이미지의 너비
+        self.screen_width = 800  # 화면 너비
 
     def draw(self):
-        self.hills.draw(0 - self.king.camera_x, 420, 1792, 328)
-        pass
+        # rocks: 1배속
+        rocks_offset = self.king.camera_x % self.image_width
+        self.rocks.draw(self.image_width - rocks_offset, self.rocks_y, self.image_width, 328)
+        self.rocks.draw(-rocks_offset + 200, self.rocks_y, self.image_width, 328)
+
+        # hills: 2배속
+        hills_offset = self.king.camera_x % self.image_width
+        self.hills.draw(self.image_width - hills_offset, self.hills_y, self.image_width, 328)
+        self.hills.draw(-hills_offset + 200, self.hills_y, self.image_width, 328)
 
     def update(self):
         pass

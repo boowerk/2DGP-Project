6d101d6a5925fa91624e9c42819786f7c99096a7
boowerk <back20044@gmail.com>
2024-12-07 22:20:11 +0900

    왕이 가까워지면 portal 활성화

--- portal.py
+++ portal.py
@@ -1,5 +1,7 @@
 from pico2d import load_image
 
+import game_framework
+
 
 class Portal:
     image = None
@@ -12,8 +14,31 @@ class Portal:
         self.king = king
         self.frame = 0
         self.col = 8
+        self.frame_timer = 0
+        self.distance = 0
 
     def update(self):
+        self.distance = abs(self.king.x - self.x)
+
+        if self.distance <= 400:
+            self.frame_timer += game_framework.frame_time
+            if self.frame_timer >= 0.1:  # 프레임 간격을 0.1초로 설정 (필요에 따라 조정 가능)
+                self.frame = (self.frame + 1) % 5
+                self.frame_timer = 0.0  # 타이머 리셋
+                if self.frame == 4:
+                    self.frame = 0
+
+                    if self.col == 4:
+                        self.col = 6
+                        self.frame = 0
+                    else:
+                        self.col -= 1
+        else:
+            self.frame = 0
+            self.col = 8
+
+
+
         pass
 
     def draw(self):

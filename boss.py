efb166e9d9c95c918a86343c7fd027617a4dcd72
boowerk <back20044@gmail.com>
2024-12-08 01:57:11 +0900

    boss의 공격으로 벽이 부숴지지 않는 버그 수정

--- boss.py
+++ boss.py
@@ -12,7 +12,7 @@ from wall import Wall
 
 # troll Run Speed
 PIXEL_PER_METER = (10.0 / 0.3)
-RUN_SPEED_KMPH = 12.0
+RUN_SPEED_KMPH = 8.0
 RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
@@ -37,7 +37,7 @@ class Walk:
         walls = game_world.find_objects(Wall)  # Wall 객체 리스트 가져오기
         for wall in walls:
             distance = abs(boss.x - wall.x)
-            if distance < 50 and wall.hp > 0:  # 벽과 100 이하의 거리일 경우 이벤트 발생
+            if distance < 100 and wall.hp > 0:  # 벽과 100 이하의 거리일 경우 이벤트 발생
                 boss.state_machine.add_event(('FIND_WALL', 0))
 
         boss.x += boss.dir * RUN_SPEED_PPS * game_framework.frame_time
@@ -76,12 +76,14 @@ class Attack:
     def do(boss):
         boss.frame_timer += game_framework.frame_time
         boss.attack_timer += game_framework.frame_time  # 공격 타이머 갱신
+        boss.hand_timer += game_framework.frame_time
+
 
         # 일정 시간마다 공격 실행 (예: 1초)
         if boss.attack_timer >= 5.0:
             walls = game_world.find_objects(Wall)  # Wall 객체 리스트 가져오기
             for wall in walls:
-                if abs(boss.x - wall.x) < 100:  # 가까운 Wall에만 공격
+                if abs(boss.x - wall.x) < 150:  # 가까운 Wall에만 공격
                     wall.take_damage(1)  # HP 1씩 감소
                     print(f"Wall HP: {wall.hp}")
                     if wall.hp <= 0:
@@ -160,17 +162,18 @@ class Boss:
         self.col = 3
         self.frame_timer = 0
         self.attack_timer = 0
+        self.hand_timer = 0
         self.king = king
         self.use_left_hand = True  # True면 왼손, False면 오른손
         self.damaged = False
         self.damaged_timer = 0
-        self.hp = 3
+        self.hp = 5
         self.attack_left_image = load_image('boss_bash_left.png')
         self.attack_right_image = load_image('boss_bash_right.png')
         self.walk_image = load_image('boss_walk.png')
         self.die_image = load_image('boss_death.png')
         self.state_machine = StateMachine(self)
-        self.state_machine.start(Attack)
+        self.state_machine.start(Walk)
         self.state_machine.set_transitions(
             {
                 Walk: {find_wall_event: Attack, die_event: Die},
@@ -194,9 +197,9 @@ class Boss:
                 self.damaged_timer = 0
 
         # 공격 간격마다 손 교체
-        if self.attack_timer >= 1.0:  # 1초마다 손을 바꿔 공격
+        if self.hand_timer >= 1.0:  # 1초마다 손을 바꿔 공격
             self.use_left_hand = not self.use_left_hand  # 손 교체
-            self.attack_timer = 0  # 타이머 리셋
+            self.hand_timer = 0  # 타이머 리셋
 
         if self.hp <= 0:
             self.state_machine.add_event(('DIE', 0))
@@ -208,7 +211,7 @@ class Boss:
         return self.x - self.king.camera_x - 20, self.y - 50, self.x - self.king.camera_x + 20, self.y + 20
 
     def handle_collision(self, group, other):
-        if group == 'troll:arrow' and not self.damaged:  # 다른 Troll과는 독립적으로 처리
+        if group == 'boss:arrow' and not self.damaged:  # 다른 Troll과는 독립적으로 처리
             self.damaged = True  # 충돌 상태로 설정
             if self.hp > 0:  # HP 감소
                 self.hp -= 1
